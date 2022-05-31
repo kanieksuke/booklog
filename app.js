e51cd28064f08e24e0b8eded744aea1bc1e1e1bb
kanieksuke <kanieksuke@yahoo.co.jp>
2022-05-31 11:21:17 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 3000
+let booklog = {}
+
+app.use(express.json())
+
+app.post('/booklog', (req, res) => {
+   booklog = req.body
+
+  if (!(booklog.name && booklog.text)) {
+    return res.json({
+      "status": false,
+      "error": "invalid parameter"
+    })
+  }
+
+  res.json({
+    "ok": true,
+    "booklog": booklog
+  })
+})
+
+app.get("/booklog", (req, res) => {
+  res.json({
+    "status": true,
+    "booklog": [
+      booklog
+    ]
+  })
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:$
+  {port}`)
+})
